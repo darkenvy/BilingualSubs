5cad35c3980a0fb068314faa8b05d36fc4010731
darkenvy <darkenvy6@gmail.com>
2017-02-11 18:39:59 -0800

    lined up subtitles

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var subB = parser(fs.readFileSync(__dirname + '/ja.ass', 'utf8'), {comments: tru
 var timecode = {
   fromFloat: function() {},
   toFloat: function(timecodeFormat) {
-    console.log(timecodeFormat);
     var regx = timecodeFormat.match(/(\d+):(\d{1,2}):(\d{1,2})\.(\d{1,2})/);
     var hours        = parseInt(regx[1]) * 60 * 60 * 100,
         minutes      = parseInt(regx[2]) * 60 * 100,
@@ -15,7 +14,17 @@ var timecode = {
     return (hours + minutes + seconds + milliseconds) / 100;
   },
   diff: function(one, two) {
-    return Math.abs(this.toFloat(one) - this.toFloat(two)).toFixed(2);
+    return Math.abs(one - two).toFixed(2);
+  },
+  center: function(start, end) {
+    return (parseFloat(
+          ((parseFloat(end) - parseFloat(start))/2).toFixed(2))
+          + parseFloat(start)).toFixed(2);
+  },
+  isBetween: function(start, end, query) {
+    var t = (query > start && query <= end) ? true : false;
+    // console.log('t: ', t, start, query, end);
+    return t
   }
 }
 
@@ -24,50 +33,48 @@ var timecode = {
 // |             Main              |
 // +-------------------------------+
 
-var reSubA = [];
+// Subtitles where the center isn't coinside with another sub, are ignored.
+
+var reSub = [];
 
 for (var i in subA[2].body) {
-  if (i == 0) continue;
-  console.log(i);
-  reSubA.push({
-    start: subA[2].body[i].value.Start,
-    end: subA[2].body[i].value.End,
-    duration: timecode.diff(subA[2].body[i].value.Start, subA[2].body[i].value.End),
-    // preMargin: (function() {
-    //   if (parseInt(i) > 1 
-    //     && subA[2].body[ i ].hasOwnProperty('value') 
-    //     && subA[2].body[ i ].value.Start !== undefined 
-    //     && subA[2].body[i-1].hasOwnProperty('value') 
-    //     && subA[2].body[i-1].value.End !== undefined) {
-    //       return timecode.diff(subA[2].body[ i ].value.Start, subA[2].body[i-1].value.End);
-    //   } 
-    //   else {return null;}
-    // })(),
-    postMargin: (function() {
-      console.log(i, i+1, subA[2].body.length, subA[2].body.length-1);
-      if (parseInt(i)+1 < subA[2].body.length-1 
-        && subA[2].hasOwnProperty('body')
-        && subA[2].body !== undefined
-        && subA[2].body[i+1].hasOwnProperty('value') 
-        && subA[2].body[i+1].value.Start !== undefined 
-        && subA[2].body[ i ].hasOwnProperty('value') 
-        && subA[2].body[ i ].value.End !== undefined) {
-          // console.log('HERE: ', subA[2].body[i+1].value.Start, subA[2].body[i+1].value.End);
-          return timecode.diff(subA[2].body[i+1].value.Start, subA[2].body[i].value.End);
-      } 
-      else {return null;}
-    })(),
-    // preMargin: parseInt(i) <= 1 ? null :
-    //   timecode.diff(
-    //     subA[2].body[ i ].value.Start, 
-    //     subA[2].body[i-1].value.End),
-    // postMargin: parseInt(i)+1 >= subA[2].body.length ? null :
-    //   timecode.diff(
-    //     subA[2].body[i+1].value.Start, 
-    //     subA[2].body[ i ].value.End),
-    text: subA[2].body[i].value.Text
+  if (i == 0 || !subA[2].body[i].hasOwnProperty('value')) continue;
+  var current = subA[2].body[i],
+      start   = timecode.toFloat(current.value.Start),
+      end     = timecode.toFloat(current.value.End),
+      text    = current.value.Text;
+  reSub.push({
+    start: start,
+    end: end,
+    duration: timecode.diff(start, end),
+    text1: text,
+    text2: null
   })
-  console.log(reSubA[reSubA.length-1], subA[2].body.length);
+  // console.log(reSub[reSub.length-1], subA[2].body.length);
+}
+
+var previousFind = 0;
+for (var i in subB[2].body) {
+  if (i == 0 || !subB[2].body[i].hasOwnProperty('value')) continue;
+  var current = subB[2].body[i],
+      start   = timecode.toFloat(current.value.Start),
+      end     = timecode.toFloat(current.value.End),
+      center  = timecode.center(start, end),
+      text    = current.value.Text;
+  if (center == 0.00 || (!text && text.length == 0) ) continue;
+
+  for (var j = previousFind; j < reSub.length; j++) {
+    if (timecode.isBetween(reSub[j].start, reSub[j].end, center)) {
+      reSub[j].text2 = text;
+      previousFind = j;
+      break;
+    }
+  }
+
+  console.log(reSub);
+
+
+
 }
 
 // console.log(subA[2].body[5].value);
